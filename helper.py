2aecf7ea2cd931e394c255d621a6f28a307f0513
007kakashi <upendrasgosavi@gmail.com>
2024-01-13 13:13:46 +0530

    Project Completed

---
+++ helper.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from helper import get_few_shot_ans
+
+st.header('Chat With Database')
+
+input = st.text_input('Ask Your Queries')
+
+answer = st.button('Answer')
+
+if answer:
+    ans = get_few_shot_ans()
+    get_ans = ans.run(input)
+    st.write(get_ans)
